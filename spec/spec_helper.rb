4bb789fba846b081f901626d5d66d6cfd06e7405
Brian Freese <brianfreese@hotmail.com>
2018-01-21 16:08:57 -0600

    - Fixes depreciation for sprockets 3, limits to < sprockets 4 until work required can be completed - Adds support for haml 5

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,4 +16,16 @@ require File.expand_path(__FILE__, '../lib/hamlbars.rb')
 RSpec.configure do |config|
   config.color = true
   config.formatter = 'd'
+  filtered_specs = []
+  if Haml::VERSION <= '4.0.7'
+    filtered_specs << :haml5
+  else
+    filtered_specs << :haml4
+  end
+  if Sprockets::VERSION <= '2.12.4'
+    filtered_specs << :sprockets3
+  else
+    filtered_specs << :sprockets2
+  end
+  config.filter_run_excluding :type => lambda {|v| filtered_specs.include?(v)}
 end
